f29f7296fa3edcf3d5bf284c020fb5afed01876d
Gustavo Cruz <gustavo@pop-os.localdomain>
2020-09-29 12:32:20 -0500

    first commit: project setup

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+require("dotenv").config();
+
+//Database Connection
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database Connected"));
+
+
+//Server Port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+//Routes
+app.use(require('./routes/user'))
