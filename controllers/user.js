cdec1786e80ce617d6fe100615112c3ac7c7ecff
Gustavo Cruz <gustavo@pop-os.localdomain>
2020-10-13 19:45:53 -0500

    fix:user creation with promises

--- controllers/user.js
+++ controllers/user.js
@@ -2,15 +2,11 @@ const User = require("../models/User");
 
 exports.signup = (req, res) => {
     const user = new User(req.body);
-    user.save((err, user) => {
-        if(err) {
-            return res.status(400).json({
-                err
-            })
-        }
-
-        res.json({
-            user
+    user.save()
+        .then((user) => {
+            res.status(200).json({ user });
         })
-    })
+        .catch((err) => {
+            res.status(400).json({ err });
+        });
 };
