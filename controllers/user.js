82d99ae9b09d10d33a6248931c62eda9d4b4c11e
Gustavo Cruz <gustavo@pop-os.localdomain>
2020-10-14 15:04:30 -0500

    feature: login and logout working

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,7 @@ exports.signup = async (req, res) => {
 
     try {
         await user.save();
-        const token = await user.generateAuthToken()
+        const token = await user.generateAuthToken();
         res.status(200).json({ user, token });
     } catch (err) {
         res.status(400).json({ err });
@@ -13,13 +13,41 @@ exports.signup = async (req, res) => {
 };
 
 exports.signin = async (req, res) => {
-
     try {
-        const user = await User.findByCredentials(req.body.email, req.body.password)
-        const token = await user.generateAuthToken()
-        res.send({user, token})
+        const user = await User.findByCredentials(
+            req.body.email,
+            req.body.password
+        );
+        const token = await user.generateAuthToken();
+        res.send({ user, token });
     } catch (err) {
-        res.status(400).send()
+        res.status(400).send();
     }
-
 };
+
+exports.getMyUser = (req, res) => {
+    res.send(req.user);
+};
+
+exports.logout = async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token;
+        });
+
+        await req.user.save();
+        res.send();
+    } catch (err) {
+        res.status(500).send();
+    }
+};
+
+exports.logoutAll = async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin } = require("../controllers/user");
+const {
+    signup,
+    signin,
+    getMyUser,
+    logout,
+    logoutAll,
+} = require("../controllers/user");
+const { isAuth } = require("../middlewares/auth");
 
 router.post("/signup", signup);
 router.post("/signin", signin);
+router.get("/users/me", isAuth, getMyUser);
+router.post("/logout", isAuth, logout);
+router.post("/logoutAll", isAuth, logoutAll);
 
 module.exports = router;
