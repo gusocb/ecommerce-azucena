185775646e77bfccdc401b5071da46b4ff45cc2c
Gustavo Cruz <gustavocruzb@outlook.com>
2020-10-20 15:09:34 -0500

    fix:create products with with auth middlewares

--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const { create } = require("../controllers/category");
+const { isAuth, isAdmin } = require("../middlewares/auth");
 
-router.post("/category/create", create);
+router.post("/category/create", isAuth, isAdmin, create);
 
 module.exports = router;
